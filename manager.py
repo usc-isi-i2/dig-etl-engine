a4b0ebcd8980e279c88e7aba98b31255807d315c
greatyyx <bigyyx@gmail.com>
2017-08-10 16:20:54 -0700

    add http endpoint for manager

--- manager.py
+++ manager.py
@@ -3,7 +3,58 @@ import sys
 import subprocess
 import json
 from kafka import KafkaProducer, KafkaConsumer
+import logging
 from config import config
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+logger = logging.getLogger('mydig-webservice.log')
+log_file = logging.FileHandler('log.log')
+logger.addHandler(log_file)
+# log_file.setFormatter(logging.Formatter(config['logging']['format']))
+# logger.setLevel(config['logging']['level'])
+
+
+@app.route('/')
+def home():
+    return 'DIG ETL Engine\n'
+
+
+@app.route('/create_project', methods=['POST'])
+def create_project():
+    args = request.get_json(force=True)
+    if 'project_name' not in args:
+        return jsonify({'error_message': 'invalid project_name'}), 400
+
+    # create topics
+    ensure_topic_exists(
+        args['project_name'] + '_in', config['input_zookeeper_server'], config['input_partitions'])
+    ensure_topic_exists(
+        args['project_name'] + '_out', config['output_zookeeper_server'], config['output_partitions'])
+    return jsonify({}), 201
+
+
+@app.route('/run_etk', methods=['POST'])
+def run_etk():
+    args = request.get_json(force=True)
+    if 'project_name' not in args:
+        return jsonify({'error_message': 'invalid project_name'}), 400
+    args['number_of_workers'] = args.get('number_of_workers', 4)
+
+    kill_etk_process(args['project_name'], True)
+    run_etk_processes(args['project_name'], args['number_of_workers'])
+    return jsonify({}), 202
+
+
+@app.route('/kill_etk', methods=['POST'])
+def kill_etk():
+    args = request.get_json(force=True)
+    if 'project_name' not in args:
+        return jsonify({'error_message': 'invalid project_name'}), 400
+    kill_etk_process(args['project_name'], True)
+    return jsonify({}), 201
 
 
 def ensure_topic_exists(topic, zookeeper_server, partitions):
@@ -12,7 +63,8 @@ def ensure_topic_exists(topic, zookeeper_server, partitions):
     # --replication-factor 1
     # --partitions 4
     # --topic test
-    cmd = '{} --create --if-not-exists --zookeeper {} --replication-factor 1 --partitions {} --topic {}'.format(
+    cmd = '{} --create --if-not-exists --zookeeper {} \
+    --replication-factor 1 --partitions {} --topic {}'.format(
         os.path.join(config['kafka_bin_path'], 'kafka-topics.sh'),
         ','.join(zookeeper_server),
         partitions,
@@ -23,9 +75,24 @@ def ensure_topic_exists(topic, zookeeper_server, partitions):
         pass
 
 
+def delete_topic(topic, zookeeper_server, partitions):
+    # in broker, set `delete.topic.enable` to `true`
+    # kafka-topics.sh --delete --if-exists --zookeeper localhost:2181 --topic test
+    cmd = '{} --delete --if-exists --zookeeper {} --topic {}'.format(
+        os.path.join(config['kafka_bin_path'], 'kafka-topics.sh'),
+        ','.join(zookeeper_server),
+        topic
+    )
+    ret = subprocess.call(cmd, shell=True)
+    if ret != 0:
+        pass
+
+
 def run_etk_processes(project_name, processes):
+
     for i in xrange(processes):
-        cmd = 'python -u {run_core_path} \
+        cmd = 'source activate etk_env; \
+        python -u {run_core_path} \
         --tag-mydig-etk-{project_name}-{idx} \
         --config "{working_dir}/etk_config.json" \
         --kafka-input-server "{input_server}" \
@@ -34,7 +101,7 @@ def run_etk_processes(project_name, processes):
         --kafka-input-session-timeout {session_timeout} \
         --kafka-output-server "{output_server}" \
         --kafka-output-topic "{project_name}_out" \
-        > "{working_dir}/etk_stdout_{idx}.txt" &'.format(
+        > "{working_dir}/etk_stdout_{idx}.txt"'.format(
             run_core_path=os.path.join(config['etk_path'], 'etk/run_core.py'),
             project_name=project_name,
             working_dir=os.path.join(config['projects_path'], project_name, 'working_dir'),
@@ -44,50 +111,19 @@ def run_etk_processes(project_name, processes):
             input_group_id=config['input_group_id'],
             idx=i
         )
-        ret = subprocess.call(cmd, shell=True)
-        if ret != 0:
-            pass
+        print cmd
+        subprocess.Popen(cmd, shell=True) # async
+
+    print 'finish'
 
 
 def kill_etk_process(project_name, ignore_error=False):
     cmd = 'ps -ef | grep -v grep | grep "tag-mydig-etk-{}" | awk \'{{print $2}}\' | xargs kill -9'.format(project_name)
     ret = subprocess.call(cmd, shell=True)
     if ret != 0 and ignore_error:
-        pass
+        print 'error'
+    return 'finish'
 
-
-def clean_data(project_name):
-    pass
 
 if __name__ == '__main__':
-    # input init
-    ensure_topic_exists(config['manager_topic'], config['input_zookeeper_server'], 1) # only need 1 partition
-    consumer = KafkaConsumer(
-        bootstrap_servers=config['input_server'],
-        value_deserializer=lambda v: json.loads(v.decode('utf-8')))
-    consumer.subscribe([config['manager_topic']])
-
-    # output init
-    producer = KafkaProducer(
-        bootstrap_servers=config['output_server'],
-        value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
-    # interpret
-    for msg_obj in consumer:
-        try:
-            msg = msg_obj.value
-            if msg['cmd'] == 'create_project':
-                # create topic
-                ensure_topic_exists(msg['project_name'] + '_in',
-                                    config['input_zookeeper_server'], config['input_partitions'])
-                ensure_topic_exists(msg['project_name'] + '_out',
-                                    config['output_zookeeper_server'], config['output_partitions'])
-            elif msg['cmd'] == 'run_etk':
-                kill_etk_process(msg['project_name'], True)
-                run_etk_processes(msg['project_name'], msg['number_of_workers'])
-            elif msg['cmd'] == 'kill_etk':
-                kill_etk_process(msg['project_name'])
-            else:
-                raise Exception('unknown command')
-        except Exception as e:
-            print e
+    app.run(debug=config['debug'], host=config['server']['host'], port=config['server']['port'], threaded=True)
