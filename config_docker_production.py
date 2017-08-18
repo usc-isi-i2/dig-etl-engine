b5e0ef46af0ce933d00348ee3279d4731fe79962
greatyyx <bigyyx@gmail.com>
2017-08-18 15:24:53 -0700

    add production compose and config

---
+++ config_docker_production.py
@@ -0,0 +1,32 @@
+import logging
+
+config = {
+    'debug': True,
+    'server': {
+        'host': '0.0.0.0',
+        'port': 9999,
+    },
+
+    'logstash': {
+        'host': 'logstash',
+        'port': 5959,
+        'level': logging.INFO,
+        'version': 1,
+        'name': 'dig_etl_engine'
+    },
+
+    'input_zookeeper_server': ['zookeeper:2181'],
+    'output_zookeeper_server': ['zookeeper:2181'],
+    'input_server': ['kafka:9092'],
+    'output_server': ['kafka:9092'],
+
+    # per project topic
+    'input_partitions': 4,
+    'output_partitions': 4,
+    'input_session_timeout': 60*60*1000,
+    'input_group_id': 'dig',
+
+    'kafka_bin_path': '/app/kafka/bin',
+    'projects_path': '/shared_data/projects',
+    'etk_path': '/app/etk'
+}
