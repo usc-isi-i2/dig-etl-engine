bcf9045ff7b435773d034846c22cc2615f8f740a
greatyyx <bigyyx@gmail.com>
2017-12-14 11:04:46 +0800

    support docker-compose config in start script

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ cmd_suffix="up"
 for arg in $@; do
     if [ "$arg" == "-d" ]; then
         cmd_suffix="up -d"
+    elif [ "$arg" == "config" ]; then
+        cmd_suffix="config"
     else
         cmd="$cmd -f docker-compose.${arg}.yml"
     fi
