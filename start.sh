4d0bc55449d55441b45f3f959c9081884231c2b4
greatyyx <bigyyx@gmail.com>
2017-12-14 11:46:37 +0800

    update input format of command in start script

--- start.sh
+++ start.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
+# usage:
+# ./start.sh up
+# ./start.sh +dev up
+# ./start.sh +dev +ache up -d
+# ./start.sh +dev config
+# ./start.sh +dev down
+# ./start.sh +dev restart mydig_ws
+
 cmd="docker-compose -f docker-compose.yml"
-cmd_suffix="up"
 
 for arg in $@; do
-    if [ "$arg" == "-d" ]; then
-        cmd_suffix="up -d"
-    elif [ "$arg" == "config" ]; then
-        cmd_suffix="config"
-    else
+    if [[ "${arg:0:1}" == "+" ]]; then
+        arg=${arg:1} # remove plus sign
         cmd="$cmd -f docker-compose.${arg}.yml"
+    else
+        cmd="$cmd ${arg}"
     fi
 done
 
-cmd="$cmd $cmd_suffix"
 #echo $cmd
 eval $cmd
