044c7b5f61e68d6607760a9c8b8c932bef010c6a
greatyyx <bigyyx@gmail.com>
2018-01-10 10:52:12 -0800

    support getting add-on information from env file

--- engine.sh
+++ engine.sh
@@ -10,6 +10,13 @@
 
 cmd="docker-compose -f docker-compose.yml"
 
+# add parameter from env file
+source ./.env
+for arg in $(echo $DIG_ADD_ONS | tr "," "\n"); do
+    cmd="$cmd -f docker-compose.${arg}.yml"
+done
+
+# add parameter from command line
 for arg in $@; do
     if [[ "${arg:0:1}" == "+" ]]; then
         arg=${arg:1} # remove plus sign
