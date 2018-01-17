eddbcd198167ee20ad656a42e25d5cb1d3d37fa9
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-01-16 19:38:02 -0800

    add code to decode values and added unit test

---
+++ utilities/tests/test_decode_dict.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import unittest
+import sys, os
+import json
+
+sys.path.insert(0, '../')
+
+from data_import.dig_tabular_import import TabularImport
+
+
+class TestDeleteCellValues(unittest.TestCase):
+    def setUp(self):
+        self.csv_file = os.path.join(os.path.dirname(__file__), "./test_data/delete_cell_input")
+
+    def test_decode_cell(self):
+        mapping_spec = {
+            "prefix": "testdecode",
+            "website": "http://testdecode.isi",
+            "file_url": "https://testdecode.isi/1",
+            "id_path": "",
+            "remove_leading_trailing_whitespace": True,
+            "remove_blank_fields": True,
+            "config": {
+                "title": "{A}: decode test  in {C}",
+                "type": [
+                    "Event",
+                    "Decode Test"
+                ],
+                "rules": [
+                    {
+                        "path": "B 1",
+                        "field": "decoded",
+                        "decoding_dict": {
+                            "is": "are"
+
+                        }
+                    }
+                ]
+            }
+        }
+        ti = TabularImport(self.csv_file, mapping_spec)
+        objs = ti.object_list
+        self.assertTrue(objs[0]['B 1'], 'are')
+        self.assertTrue(objs[1]['B 1'], 'are')
+
+
+if __name__ == '__main__':
+    unittest.main()
