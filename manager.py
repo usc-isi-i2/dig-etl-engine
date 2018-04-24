d61dcb6cf423a2af0938d005b569f44fd05acbe5
greatyyx <bigyyx@gmail.com>
2018-04-24 12:29:30 -0700

    new etk worker support

--- manager.py
+++ manager.py
@@ -18,11 +18,16 @@ app = Flask(__name__)
 # logging
 logger = logging.getLogger(config['logstash']['name'])
 logger.setLevel(config['logstash']['level'])
+# logging.Formatter(config['logging']['format'])
+log_stdout = logging.StreamHandler(sys.stdout)
+# log_stdout.setFormatter(log_formatter)
+logger.addHandler(log_stdout)
 # logger.addHandler(
 #     logstash.LogstashHandler(
 #         config['logstash']['host'], config['logstash']['port'], version=config['logstash']['version']))
-logger.addHandler(logging.FileHandler('log.log'))
-# logging.getLogger('werkzeug').setLevel(logging.ERROR) # turn off werkzeug logger for normal info
+logger.setLevel(logging.INFO)
+logging.getLogger('werkzeug').setLevel(logging.ERROR)  # turn off werkzeug logger for normal info
+
 
 @app.route('/')
 def home():
@@ -31,29 +36,19 @@ def home():
 
 @app.route('/create_project', methods=['POST'])
 def create_project():
+    """
+    make sure auto.create.topics.enable=true in kafka
+    """
     args = request.get_json(force=True)
     if 'project_name' not in args:
         return jsonify({'error_message': 'invalid project_name'}), 400
 
-    config_path = os.path.join(config['projects_path'], args['project_name'], 'working_dir/etl_config.json')
-    project_config = {}
-    if os.path.exists(config_path):
-        with open(config_path, 'r') as f:
-            project_config = json.loads(f.read())
-
-    # # create topics
-    # input_topic = project_config.get('input_topic', args['project_name'] + '_in')
-    # input_zookeeper_server = project_config.get('input_zookeeper_server', config['input_zookeeper_server'])
-    # input_partition = project_config.get('input_partitions', config['input_partitions'])
-    # ensure_topic_exists(input_topic, input_zookeeper_server, input_partition)
-
-    output_topic = project_config.get('output_topic', args['project_name'] + '_out')
-    # output_zookeeper_server = project_config.get('output_zookeeper_server', config['output_zookeeper_server'])
-    output_partition = project_config.get('output_partitions', config['output_partitions'])
-    # ensure_topic_exists(output_topic, output_zookeeper_server, output_partition)
+    etl_config = get_project_etl_config(args['project_name'])
+    output_topic = etl_config.get('output_topic', args['project_name'] + '_out')
+    output_partition = etl_config.get('output_partitions', config['output_partitions'])
 
     # update logstash pipeline
-    output_server = project_config.get('output_server', config['output_server'])
+    output_server = etl_config.get('output_server', config['output_server'])
     if config['version'] == 'sandbox':
         update_logstash_pipeline(
             args['project_name'], output_server, output_topic, output_partition)
@@ -68,21 +63,16 @@ def run_etk():
         return jsonify({'error_message': 'invalid project_name'}), 400
     args['number_of_workers'] = args.get('number_of_workers')
 
-    config_path = os.path.join(config['projects_path'], args['project_name'], 'working_dir/etl_config.json')
-    project_config = {}
-    if os.path.exists(config_path):
-        with open(config_path, 'r') as f:
-            project_config = json.loads(f.read())
+    etl_config = get_project_etl_config(args['project_name'])
 
-    kill_and_clean_up_queue(args, project_config)
+    kill_and_clean_up_queue(args, etl_config)
 
-    run_etk_processes(args['project_name'], args['number_of_workers'], project_config)
+    run_etk_processes(args['project_name'], args['number_of_workers'], etl_config)
     return jsonify({}), 202
 
 
 @app.route('/kill_etk', methods=['POST'])
 def kill_etk():
-
     args = request.get_json(force=True)
     if 'project_name' not in args:
         return jsonify({'error_message': 'invalid project_name'}), 400
@@ -98,8 +88,42 @@ def kill_etk():
     return jsonify({}), 202
 
 
-def kill_and_clean_up_queue(args, project_config):
+@app.route('/etk_status/<project_name>', methods=['GET'])
+def etk_status(project_name):
+    cmd = 'ps -ef | grep -v grep | grep "tag-mydig-etk-{project_name}" | wc -l'.format(project_name=project_name)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    output = p.stdout.read()
+    try:
+        process_num = int(output) / 2
+        return jsonify({'etk_processes': process_num})
+    except:
+        logger.exception('etk_status error: {}'.format(project_name))
+        return 'error', 500
 
+
+@app.route('/debug/ps', methods=['GET'])
+def debug_ps():
+    p = subprocess.Popen('ps -ef | grep -v grep | grep "tag-mydig-etk"', stdout=subprocess.PIPE, shell=True)
+    output = p.stdout.read()
+    return output, 200
+
+
+def get_project_etl_config(project_name):
+    """
+    get project level etl config
+    """
+    config_path = os.path.join(config['projects_path'], project_name, 'working_dir/etl_config.json')
+    etl_config = dict()
+    if os.path.exists(config_path):
+        with open(config_path, 'r') as f:
+            etl_config = json.loads(f.read())
+    return etl_config
+
+
+def kill_and_clean_up_queue(args, project_config):
+    """
+    Kill ETK processes, set kafka offset pointer to the end
+    """
     kill_etk_process(args['project_name'], True)
 
     # reset input offset in `dig` group
@@ -113,135 +137,45 @@ def kill_and_clean_up_queue(args, project_config):
         seek_to_topic_end(args['project_name'] + '_out', output_partitions,
                           config['output_server'])
 
-# @app.route('/delete_topics', methods=['POST'])
-# def delete_topics():
-#     args = request.get_json(force=True)
-#     if 'project_name' not in args:
-#         return jsonify({'error_message': 'invalid project_name'}), 400
-#
-#     config_path = os.path.join(config['projects_path'], args['project_name'], 'working_dir/etl_config.json')
-#     project_config = {}
-#     if os.path.exists(config_path):
-#         with open(config_path, 'r') as f:
-#             project_config = json.loads(f.read())
-#
-#     input_topic = project_config.get('input_topic', args['project_name'] + '_in')
-#     output_topic = project_config.get('output_topic', args['project_name'] + '_out')
-#     input_zookeeper_server = project_config.get('input_zookeeper_server', config['input_zookeeper_server'])
-#     output_zookeeper_server = project_config.get('output_zookeeper_server', config['output_zookeeper_server'])
-#     delete_topic(input_topic, input_zookeeper_server)
-#     delete_topic(output_topic, output_zookeeper_server)
-#     return jsonify({}), 201
-
-@app.route('/etk_status/<project_name>', methods=['GET'])
-def etk_status(project_name):
-    cmd = 'ps -ef | grep -v grep | grep "tag-mydig-etk-{project_name}" | wc -l'.format(project_name=project_name)
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
-    output = p.stdout.read()
-    try:
-        process_num = int(output) / 2
-        return jsonify({'etk_processes':process_num})
-    except:
-        logger.exception('etk_status error: {}'.format(project_name))
-        return 'error', 500
-
-@app.route('/debug/ps', methods=['GET'])
-def debug_ps():
-    p = subprocess.Popen('ps -ef | grep -v grep | grep "tag-mydig-etk"', stdout=subprocess.PIPE, shell=True)
-    output = p.stdout.read()
-    return output, 200
-
-def ensure_topic_exists(topic, zookeeper_server, partitions):
-    # kafka-topics.sh --create --if-not-exists
-    # --zookeeper localhost:2181
-    # --replication-factor 1
-    # --partitions 4
-    # --topic test
-    cmd = '{} --create --if-not-exists --zookeeper {} \
-    --replication-factor 1 --partitions {} --topic {}'.format(
-        os.path.join(config['kafka_bin_path'], 'kafka-topics.sh'),
-        ','.join(zookeeper_server),
-        partitions,
-        topic
-    )
-    ret = subprocess.call(cmd, shell=True)
-    if ret != 0:
-        logger.error('ensure_topic_exists error: {}'.format(topic))
-        return
-    logger.info('ensure_topic_exists finish: {}'.format(topic))
-
 
 def seek_to_topic_end(topic, num_partitions, consumers, group_id=None):
     consumer = KafkaConsumer(
         bootstrap_servers=consumers,
         group_id=group_id)
     # need to manually assign partitions if seek_to_end needs to be used here
-    partitions = [TopicPartition(topic, i) for i in xrange(num_partitions)]
-    consumer.assign(partitions) # conflict to subscribe
+    partitions = [TopicPartition(topic, i) for i in range(num_partitions)]
+    consumer.assign(partitions)  # conflict to subscribe
     consumer.seek_to_end()
     logger.info('seek_to_topic_end finish: {}'.format(topic))
 
 
-# def delete_topic(topic, zookeeper_server):
-#     # in broker, set `delete.topic.enable` to `true`
-#     # kafka-topics.sh --delete --if-exists --zookeeper localhost:2181 --topic test
-#     # may have side effects
-#     cmd = '{} --delete --if-exists --zookeeper {} --topic {}'.format(
-#         os.path.join(config['kafka_bin_path'], 'kafka-topics.sh'),
-#         ','.join(zookeeper_server),
-#         topic
-#     )
-#     ret = subprocess.call(cmd, shell=True)
-#     if ret != 0:
-#         logger.error('delete_topic: {}'.format(topic))
-#         return
-#     logger.info('delete_topic finish: {}'.format(topic))
-
-
 def run_etk_processes(project_name, processes, project_config):
-    for i in xrange(processes):
-        cmd = 'python -u {run_core_path} \
+    for i in range(processes):
+        cmd = 'python -u {etk_worker_path} \
         --tag-mydig-etk-{project_name}-{idx} \
-        --config "{working_dir}/etk_config.json" \
-        --kafka-input-server "{input_server}" \
-        --kafka-input-topic "{input_topic}" \
-        --kafka-input-group-id "{input_group_id}" \
-        --kafka-input-session-timeout {session_timeout} \
-        --kafka-output-server "{output_server}" \
-        --kafka-output-topic "{output_topic}" \
+        --project-name "{project_name}" \
         --kafka-input-args "{input_args}" \
         --kafka-output-args "{output_args}" \
-        --indexing \
         --worker-id "{idx}" \
-        > "{working_dir}/etk_stdout_{idx}.txt"'.format(
-            # os.path.join(config['etk_path'], 'etk/run_core_kafka.py')
-            run_core_path=os.path.abspath('run_core_kafka.py'),
+        --logger-name "{logger_name}"'.format(
+            etk_worker_path=os.path.abspath('etk_worker.py'),
             project_name=project_name,
-            input_topic=project_config.get('input_topic', project_name + '_in'),
-            output_topic=project_config.get('output_topic', project_name + '_out'),
-            working_dir=os.path.join(config['projects_path'], project_name, 'working_dir'),
-            session_timeout=project_config.get('input_session_timeout', config['input_session_timeout']),
-            input_server=','.join(project_config.get('input_server', config['input_server'])),
-            output_server=','.join(project_config.get('output_server', config['output_server'])),
-            input_group_id=project_config.get('input_group_id', config['input_group_id']),
             idx=i,
             input_args=json.dumps(project_config.get('input_args', {})).replace('"', '\\"'),
-            output_args=json.dumps(project_config.get('output_args', {})).replace('"', '\\"')
+            output_args=json.dumps(project_config.get('output_args', {})).replace('"', '\\"'),
+            logger_name=config['logstash']['name']
         )
-        print cmd
-        p = subprocess.Popen(cmd, shell=True) # async
+        p = subprocess.Popen(cmd, shell=True)  # async
 
     logger.info('run_etk_processes finish: {}'.format(project_name))
-    print 'run_etk_processes finish'
 
 
 def kill_etk_process(project_name, ignore_error=False):
     cmd = 'ps -ef | grep -v grep | grep "tag-mydig-etk-{}" | awk \'{{print $2}}\' | xargs kill '.format(project_name)
     ret = subprocess.call(cmd, shell=True)
     if ret != 0 and not ignore_error:
-        print 'error in kill_etk_process'
+        logger.error('error in kill_etk_process')
     logger.info('kill_etk_process finish: {}'.format(project_name))
-    print 'kill_etk_process finish'
 
 
 def update_logstash_pipeline(project_name, output_server, output_topic, output_partition):
@@ -288,17 +222,20 @@ output {{
 
 
 def create_mappings(index_name, payload_file_path):
+    """
+    create mapping in es
+    """
     try:
         url = '{}/{}'.format(config['es_url'], index_name)
         resp = requests.get(url)
-        if resp.status_code // 100 == 4: # if no such index there
+        if resp.status_code // 100 == 4:  # if no such index there
             with codecs.open(payload_file_path, 'r') as f:
-                payload = f.read() # stringfied json
+                payload = f.read()  # stringfied json
             resp = requests.put(url, payload)
             if resp.status_code // 100 != 2:
-                print 'can not create es index for {}'.format(index_name)
+                logger.error('can not create es index for {}'.format(index_name))
             else:
-                print 'es index {} created'.format(index_name)
+                logger.error('es index {} created'.format(index_name))
     except requests.exceptions.ConnectionError:
         # es if not online, retry
         time.sleep(5)
@@ -316,8 +253,4 @@ if __name__ == '__main__':
 
         app.run(debug=config['debug'], host=config['server']['host'], port=config['server']['port'], threaded=True)
     except Exception as e:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-        lines = ''.join(lines)
-        print lines
-        logger.error(lines)
+        logger.exception('Exception in dig-etl-engine manager')
